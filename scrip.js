c284c0ad9b8b19eef869e2000641a9e155b4576e
adhilahammed <adhilahammed0@gmail.com>
2022-10-28 11:32:32 +0530

    first commit

---
+++ scrip.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 100){
+        $(".fading").css({"opacity" : "0"})
+      }
+      else {
+        $(".fading").css({"opacity" : "1"})
+      }
+    })
+  })
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 1100){
+        $(".fading-2").css({"opacity" : "0"})
+      }
+      else {
+        $(".fading-2").css({"opacity" : "1"})
+      }
+    })
+  })
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 2000){
+        $(".fading-3").css({"opacity" : "0"})
+      }
+      else {
+        $(".fading-3").css({"opacity" : "1"})
+      }
+    })
+  }) 
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 3000){
+        $(".fading-4").css({"opacity" : "0"})
+      }
+      else {
+        $(".fading-4").css({"opacity" : "1"})
+      }
+    })
+  })
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 4000){
+        $(".fading-5").css({"opacity" : "0"})
+      }
+      else {
+        $(".fading-5").css({"opacity" : "1"})
+      }
+    })
+  })
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 5000){
+        $(".fading-6").css({"opacity" : "0"})
+      }
+      else {
+        $(".fading-6").css({"opacity" : "1"})
+      }
+    })
+  })
